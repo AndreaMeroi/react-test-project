ae974c4d69d19544459ca4030f2e3de4a1b421e3
AndreaMeroi <andreameroidublin@gmail.com>
2025-09-18 21:07:01 +0200

    Add Main and Footer error to fix

--- create-test-project/src/App.jsx
+++ create-test-project/src/App.jsx
@@ -1,12 +1,21 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+//Import Header
+import Header from './components/Header';
+
+// Import Main
+import Mail from "./components.main";
 
 function App() {
 
   return (
     <>
+      <Header></Header>
+      <Main></Main>
+      <Footer>
 
+      </Footer>
     </>
   )
 }
